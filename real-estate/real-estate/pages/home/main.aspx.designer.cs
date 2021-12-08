148753961b1529dcbd96c7eb143a5271d4230924
100231 <100231@MLN210004>
2021-12-08 08:49:06 +0700

    oat

--- real-estate/real-estate/pages/home/main.aspx.designer.cs
+++ real-estate/real-estate/pages/home/main.aspx.designer.cs
@@ -20,23 +20,5 @@ namespace real_estate.pages.home {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm mains;
-        
-        /// <summary>
-        /// drdwDistrict control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList drdwDistrict;
-        
-        /// <summary>
-        /// drdwDistrictMob control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList drdwDistrictMob;
     }
 }
